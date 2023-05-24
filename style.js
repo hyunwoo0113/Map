10df374d78df3dccdbce59518ad1db7367349869
hyunwoo Park <phw050113@gmail.com>
2023-05-24 09:23:53 +0900

    first commit

--- style.js
+++ style.js
@@ -8,3 +8,16 @@ function showModal(id) {//모달창 열기
   var modal = document.getElementById(id);
   modal.style.display = "none";
 }
+
+
+// 체크박스 요소 가져오기
+var checkbox = document.querySelector('.checkbox');
+
+// 스크롤 이벤트 리스너 추가
+window.addEventListener('scroll', function() {
+  // 현재 스크롤 위치 가져오기
+  var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+  // 체크박스 위치 업데이트
+  checkbox.style.top = (20 + scrollTop) + 'px'; // 50은 체크박스 초기 위치입니다.
+});
